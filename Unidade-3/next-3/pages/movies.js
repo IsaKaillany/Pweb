cc6de34e08343184ed4a3573d69e47be29957ddc
IsaKaillany <isakaillany123@gmail.com>
2023-11-10 23:12:49 -0300

    finish receita

--- Unidade-3/next-3/pages/movies.js
+++ Unidade-3/next-3/pages/movies.js
@@ -1,10 +1,44 @@
+import { useState } from "react";
+import styles from "../styles/movies.module.css";
+
 export default function Movies({ data }) {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const handleSearch = async () => {
+        const res = await fetch(
+            `http://www.omdbapi.com/?apikey=e0d35ba4&s=${searchQuery}`
+        );
+        const newData = await res.json();
+        // Atualiza os resultados da pesquisa no estado data
+        data.Search = newData.Search;
+    };
+
     return (
-        <div>
-            <div>
+        <div className={styles.pageContainer}>
+            <div className={styles.forms}>
+                <input
+                    type="text"
+                    placeholder="Digite sua pesquisa (Isso pode demorar)"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button className={styles.searchButton} onClick={handleSearch}>
+                    Pesquisar
+                </button>
+            </div>
+            <div className={styles.moviesContainer}>
                 {data.Search.map((m) => (
-                    <div>
-                        {m.Title} --- {m.Year}
+                    <div key={m.imdbID} className={styles.movieCard}>
+                        <a
+                            href={`https://www.imdb.com/title/${m.imdbID}/`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src={m.Poster} alt={m.Title} />
+                        </a>
+                        <div className={styles.movieText}>
+                            {m.Title} --- {m.Year}
+                        </div>
                     </div>
                 ))}
             </div>
@@ -13,8 +47,12 @@ export default function Movies({ data }) {
 }
 
 export async function getServerSideProps(context) {
-    const res = await fetch(`http://www.omdbapi.com/?apikey=de1cc0e1&s=bagdad`);
+    const { query } = context;
+    const searchQuery = query.q || "bagdad";
 
+    const res = await fetch(
+        `http://www.omdbapi.com/?apikey=e0d35ba4&s=${searchQuery}`
+    );
     const data = await res.json();
 
     return {
