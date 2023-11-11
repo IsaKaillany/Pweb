f05d1ea17e3f6352fda588b44b8ace32b519b0e3
IsaKaillany <isakaillany123@gmail.com>
2023-11-10 21:31:58 -0300

    add styles and finish

--- Unidade-3/next-2/pages/like/like.js
+++ Unidade-3/next-2/pages/like/like.js
@@ -1,7 +1,7 @@
 export function Like({ language }) {
     return (
         <p>
-            Outro framework que tenho interesse em aprender é {language}
+            Outro framework que tenho interesse em aprender é <span>{language}</span>
         </p>
     )
 }
--- Unidade-3/next-2/pages/novaPagina.js
+++ Unidade-3/next-2/pages/novaPagina.js
@@ -1,10 +1,11 @@
 import { About } from "./about/about";
 import { Img } from "./img/img";
 import { Like } from "./like/like";
+import styles from "./styles/novaPagina.module.css"
 
 export default function Principal() {
     return (
-        <div>
+        <div className={styles.container}>
             <Img/>
             <About/>
             <Like language="Node.js"/>
